991ac9f61652892e470ed4d0f458cd3f298e96f2
softprops <d.tangren@gmail.com>
2012-02-04 14:49:04 -0500

    cleaning up some stuff, more robust support for local storage

--- mu.api.js
+++ mu.api.js
@@ -1,51 +1,103 @@
 window.mu = window.mu || {};
 
+/** Provides a thin layer around an oauth2 follow for authorizing 
+ *  a client to access a Meetup member's data
+ *  @todo remove jquery dep for making ajax req 
+ *  @todo make sure this works in older browsers */
 window.mu.Api = (function(win, $) {
-    return function(opts) {
-  
+    return function(opts) {        
+
+        // if user-agent supports local storage, use that, else just use memory
+        var storageFallbacks = function() {
+            if('localStorage' in win && win['localStorage'] !== null) {
+                return {
+                    put: function(k, v) {
+                        win.localStorage[k] = v;
+                    }
+                    , get: function(k) {
+                        return win.localStorage[k];
+                    }
+                    , del: function(k) {
+                        win.removeItem(k);
+                    }
+                };
+            } else {
+                var db = {};
+                return {
+                    put: function(k, v) {
+                        db[k] = v;
+                    }
+                    , get: function(k) {
+                        return db[k];
+                    }
+                    , del: function(k) {
+                        db[k] = undefined;
+                    }
+                };
+            }
+        }
+
         // assign this to your own client id
         var client_id = opts.clientId || alert("clientId required")
 
+        storage = opts.storage || storageFallbacks()
+
+        // location to redirect member after when Meetup gets their
+        // authorization
+        , redirectUri = opts.redirectUri || window.location.href
+
         // function invoked when meetup user denies authorization 
         , onAuthDenial = opts.onAuthDenial || function(err) {
-            console.log('override onAuthDenial: function(err)...');
+            alert('override onAuthDenial: function(err)...');
         }
 
-        , onUnsupportedStorage = onUnsupportedStorage || function() {
-            console.log('override onUnsupportedStorage: function()...');
+        // function invoked when user-agent does not support localStorage
+        , onUnsupportedStorage = opts.onUnsupportedStorage || function() {
+            alert('override onUnsupportedStorage: function()...');
         }
 
+        // function invoked after a user authorizes and before
+        // onMember
         , afterAuth = opts.afterAuth || function(mem, token) {
-            console.log('override afterAuth: function(mem, token)...');
+            
         }
 
+        // function invoked on a page refresh if a user is logged in
         , onMember = opts.onMember || function(mem, token) {
-            console.log('override onMember: funciton(mem, token) ...');
+            alert('override onMember: funciton(mem, token) ...');
         }
 
-        // we support custom permission scopes
-        //http://www.meetup.com/meetup_api/auth/#oauth2-scopes
+        // support  for custom permission scopes
+        // http://www.meetup.com/meetup_api/auth/#oauth2-scopes
         , scopes = opts.scopes || ['ageless'] 
 
-        // uri for auth
-        , authorization = function(redirectUri) {
-            return "https://secure.meetup.com/oauth2/authorize/?response_type=token&client_id=" +
-                client_id + "&scope=" + scopes.join(',') + "&redirect_uri=" + redirectUri;
-        }
+        // location for auth
+        , authorization = "https://secure.meetup.com/oauth2/authorize/?response_type=token&client_id=" +
+                client_id + "&scope=" + scopes.join(',') + "&redirect_uri=" + redirectUri
 
-        // basic call to get the authorized members data
-        , member = "https://api.meetup.com/2/member/self";
+        // api call to get the authorized members data
+        , member = "https://api.meetup.com/2/member/self"
+
+        , requestAuthorization = function() {
+            var width = 500, height = 350
+            , top = (screen.height - height)/2
+            , left = (screen.width - width)/2;
+            win.open(
+                authorization,
+                "Meetup",
+                ["height=", height, ",width=", width,
+                 ",top=", top, ",left=", left].join(''));    
+        };
 
         $(function() {
       
-            // this demo stores authorization in local storage
-            if('localStorage' in win && win['localStorage'] !== null) {
-                var ls = win.localStorage;
+            if(storage) {
+                var ls = storage;
 
-                win.onMeetupAuth = function(tok) { // user okay'd auth request
-                    // cache the token for later use
-                    ls['mu_token'] = tok;
-                    $.getJSON(member + "?callback=?", { "access_token": ls['mu_token'] },
+                // user authorized client
+                win.onMeetupAuth = function(tok) {
+                    ls.put('mu_token', tok);
+                    $.getJSON(member + "?callback=?", { "access_token": ls.get('mu_token') },
                         function(mem){
                             var simple = {
                                 id: mem.id,
@@ -53,23 +105,21 @@ window.mu.Api = (function(win, $) {
                                 link: mem.link,
                                 photo: mem.photo
                             }
-                            // cache a simple, more compact, representation of the authorized user
-                            ls['mu_member'] = JSON.stringify(simple);
-                            afterAuth(simple, ls['mu_token']);
-                            onMember(simple, ls['mu_token']);
+                            ls.put('mu_member', JSON.stringify(simple));
+                            afterAuth(simple, tok);
+                            onMember(simple, tok);
                         });
-                }
+                };
 
-                win.onMeetupDenial = function(err) { // user denied auth request
+                // user denied client authorization
+                win.onMeetupDenial = function(err) {
                     onAuthDenial(err);
                 };
 
-                if(!ls['mu_token'] || !ls['mu_member']) { // if we are not "logged in"..
-              
-                    // oauth2 tokens are passed back to the redirect url in the form of
-                    // a url fragment set of params
-                    if(window.location.hash) {
-                        var fp = window.location.hash.substring(1).split('&')
+                if(!ls.get('mu_token') || !ls.get('mu_member')) { // not "logged in"..
+                    var frag = window.location.hash;
+                    if(frag) {
+                        var fp = frag.substring(1).split('&')
                         , i = fp.length
                         , params = {}
                         , re = /(\S+)=(\S+)/
@@ -89,33 +139,23 @@ window.mu.Api = (function(win, $) {
                         }
                     }
 
-                    var width = 500, height = 350
-                    , top = (screen.height - height)/2
-                    , left = (screen.width - width)/2;
-                    // in a popup...
-                    // 1) redirect the user to meetup & tell meetup where to redirect afterwards
-                    // 2) set some window title
-                    // 3) set popup args (centering window)
-                    win.open(
-                        authorization(window.location.href), // 1
-                        "Meetup",                             // 2
-                        ["height=", height, ",width=", width, // 3
-                         ",top=", top, ",left=", left].join(''));
                 } else { // we are "logged in"...
-                    onMember(JSON.parse(ls['mu_member']), ls['mu_token']);
-                    console.log('call api.logout(funciton() ...) to logout');
+                    onMember(JSON.parse(ls.get('mu_member')), ls.get('mu_token'));
                 }
             } else {
                 onUnsupportedStorage();         
             }
         });
     
-        // return a means of logging out
+        // return a means of logging out and in
         return {
             logout: function(after) {
                 win.localStorage.removeItem('mu_auth');
                 win.localStorage.removeItem('mu_member');
-                after();
+                if(after) { after(); }
+            },
+            login: function() {
+                requestAuthorization();
             }
         };
     };
